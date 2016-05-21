dfdea37538d5e08a29922f56fdcb9f089a4d581d
Steve <stephenjsweeney@battleforthesolarsystem.com>
2016-05-21 19:02:20 +0100

    Removed deb and rpm building. Mostly broken, and layout not consistent with other Linux distros.

--- build/linux/build.sh
+++ build/linux/build.sh
@@ -3,7 +3,6 @@
 cd `dirname $0`
 
 BUILDROOT="build/linux"
-OUT="$BUILDROOT/tbftss"
 
 cd ../..
 
@@ -14,29 +13,5 @@ SIZE=0
 make clean
 make
 make dist
-
-mkdir -p $OUT
-rm -rf $OUT/*
-
-mkdir -p $OUT/DEBIAN
-
-make DESTDIR=$OUT install
-
-SIZE=`du -bs $OUT | cut -f -1`
-SIZE=`expr $SIZE / 1024`
-
-sed \
--e "s/\${version}/$VERSION-$REVISION/" \
--e "s/\${size}/$SIZE/" \
-$BUILDROOT/control > $OUT/DEBIAN/control
-
-cd $BUILDROOT
-
-dpkg-deb --build tbftss
-
-mv tbftss.deb tbftss-${VERSION}-${REVISION}.i386.deb
-alien -r --bump=0 tbftss-${VERSION}-${REVISION}.i386.deb
-
-mv *.deb *.rpm ../../dist
 
 rm -rf tbftss

57bc5c44379f30cdad617d69f4d2dee00eb24931
Steve <stephenjsweeney@battleforthesolarsystem.com>
2016-08-10 13:04:45 +0100

    Version info update.

--- src/game/title.c
+++ src/game/title.c
@@ -182,7 +182,7 @@ static void draw(void)
 	blit(pandoranWar, SCREEN_WIDTH / 2, 110, 1);
 	
 	drawText(10, SCREEN_HEIGHT - 25, 14, TA_LEFT, colors.white, "Copyright Parallel Realities, 2015-2016");
-	drawText(SCREEN_WIDTH - 10, SCREEN_HEIGHT - 25, 14, TA_RIGHT, colors.white, "Version %.2f.%d", VERSION, REVISION);
+	drawText(SCREEN_WIDTH - 10, SCREEN_HEIGHT - 25, 14, TA_RIGHT, colors.white, "Version %.2f-%d", VERSION, REVISION);
 	
 	switch (show)
 	{

cc75a8d1449cb212c2c6cf2ffbede90b6c4c91a5
Steve <stephenjsweeney@battleforthesolarsystem.com>
2018-12-08 19:12:17 +0000

    Don't translate Pandoran messages.

--- src/battle/script.c
+++ src/battle/script.c
@@ -195,7 +195,7 @@ static void executeNextLine(ScriptRunner *runner)
 	else if (strcmp(command, "PANDORAN_MSG_BOX") == 0)
 	{
 		sscanf(line, "%*s %255[^;]%*c%255[^\n]", strParam[0], strParam[1]);
-		addMessageBox(strParam[0], _(strParam[1]), MB_PANDORAN);
+		addMessageBox(strParam[0], strParam[1], MB_PANDORAN);
 	}
 	else if (strcmp(command, "WAIT") == 0)
 	{

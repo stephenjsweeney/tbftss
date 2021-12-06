fc09a7d5980a539a76b2bf961f97bc724ed5814c
Steve <stephenjsweeney@battleforthesolarsystem.com>
2021-12-06 21:44:12 +0000

    Set trophyId array size to MAX_NAME_LENGTH * 2.

--- src/game/trophies.c
+++ src/game/trophies.c
@@ -426,8 +426,7 @@ void awardStatsTrophies(void)
 
 void awardCampaignTrophies(void)
 {
-	/* add 9 to MAX_NAME_LENGTH to account for "CAMPAIGN_" */
-	char trophyId[MAX_NAME_LENGTH+9];
+	char trophyId[MAX_NAME_LENGTH * 2];
 	char name[MAX_NAME_LENGTH];
 	int i, len;
 	StarSystem *starSystem;
